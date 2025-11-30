21f3c99367cc806acc80de7eabdb04f856622108
Mkobilan <matthew.kobilan@gmail.com>
2025-11-30 21:34:24 +0600

    middleware fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -27,6 +27,19 @@ export async function middleware(request: NextRequest) {
                     })
                     cookiesToSet.forEach(({ name, value, options }) =>
                         response.cookies.set(name, value, options)
+                    )
+                },
+            },
+        }
+    )
+
+    await supabase.auth.getUser()
+
+    return response
+}
+
+export const config = {
+    matcher: [
         '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 }
